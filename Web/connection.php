c93edfefbe2406202a9217f746fbb099a51b94f0
yosuef alkhatib <yousefalkhatib01@gmail.com>
2022-08-27 19:54:08 +0300

    connect to cleardb

--- Web/connection.php
+++ Web/connection.php
@@ -1,13 +1,12 @@
 <?php
-
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "gymusers";
-$dbport = 3307;
-
-
-// connect to the data base
-if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $dbport)) { // if the connection fails it returns an error otherwise it return $con
-    die("failed to connect to the database");
-};
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
